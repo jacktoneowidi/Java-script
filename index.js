699ac72c39c601fe157a5dcb30cd72c9344fdebe
jacktone owidi <kingsowidi@gmail.com>
2024-02-10 12:25:09 +0300

    learning javascript

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log('hello world')
+/*this is a 
+comment and the computer will ignore this*/
+console.log(1 + 1);
+
+console.log((3 + 2) * 4 );
+
+
+console.log(7 % 10)
+console.log(4 + 2.5)   // 6.5
+
+// Write 5 more expressions below:
+console.log(1 + 1 )
+console.log( 2 - 10)
+console.log(10 * 20 )
+console.log( 4 / 10)
+console.log(10 % 30)
+console.log(6 / 5)
+console.log((2 + 4) * 5);
+console.log(25 % 7);
+console.log(7 % 25);
+/*multi line comment*/fetch
